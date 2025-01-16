355d8b25cc260ba507d9c857cf44fb12b60829e1
stefan-git <stefan-schinkel@hotmail.com>
2025-01-16 14:33:53 +0100

    aufgabenliste zu eigenen componenten gemacht + undo button

--- react-app1/src/componets/MeineAufgaben.jsx
+++ react-app1/src/componets/MeineAufgaben.jsx
@@ -1,62 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../assets/MeineAufgabe.css'
-import AufgabenDaten from '../modules/AufgabenNutzer';
+import AufgabenListe from './AufgabenListe';
 
-function MeineAufgaben(id){
-
-  const aufgabenDaten = new AufgabenDaten();
-  const [aufgaben, setAufgaben] = useState();
-
-  useEffect(() => {
-    fetch(`http://localhost:5000/api/v1/zeiterfassungsDB/aufgaben/${id.selectedItemId}`)
-    .then(response => { 
-      if (!response.ok) { 
-        throw new Error("Network response was not ok"); 
-      }
-      return response.json()
-    })
-      .then(data => {
-        setAufgaben(data);
-    })
-    .catch(error => console.log(error));
-  }, [id.selectedItemId]);
-  
-  if (aufgaben && aufgaben.length > 0) {
-    aufgabenDaten.update(aufgaben[0]);
-  }
-  
-  function deleteAufgabe(e) {
-    const index = e.target.getAttribute('value');
-    aufgabenDaten.Aufgaben.splice(index, 1);
-    console.log(e.target);
-    e.target.remove();  
-  }
+function MeineAufgaben({selectedItemId}){
 
   return(
     <div className="info-item-meine-aufgaben">
       <div className="profile-card-meine-aufgaben">
         <div className="Name js-sprache"><p>Meine Aufgaben</p></div>  
-        <div className="aufgabenliste" style={{ maxHeight: '300px', overflowY: 'auto' }}>
-          <ul>
-            {aufgabenDaten.Aufgaben.map((todo, index) => (               
-              <li  className='Aufgabenlist'
-              key={index} onClick={deleteAufgabe} value={index} >{index+1}. {todo}</li>              
-            ))}
-            <li>
-              <input 
-                type="text" 
-                placeholder="Neue Aufgabe hinzufügen" 
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter' && e.target.value.trim() !== '') {
-                   aufgabenDaten.Aufgaben.push(e.target.value.trim());
-                   setAufgaben([aufgabenDaten.Aufgaben]);
-                   e.target.value = '';
-                   }
-                 }} 
-              />
-            </li>
-          </ul>
-        </div>
+        <AufgabenListe selectedItemId={selectedItemId}/>
         <div className="actions-meine-aufgaben">
           <a className='js-sprache' href="#">Aufgaben Übersicht öffnen</a>
         </div>    
--- react-app1/src/modules/AufgabenNutzer.js
+++ react-app1/src/modules/AufgabenNutzer.js
@@ -1,16 +1,25 @@
 class AufgabenNutzer {
   constructor() {
-    
+    this.speicher = [];
     this.Aufgaben = [];    
   }
 
   update(data) {
     data.forEach(element => {
       if (element.aufgabe === undefined) {
-        this.Aufgaben.push(element);
+        this.Aufgaben.push(element);        
       }else this.Aufgaben.push(element.aufgabe);
       
     });
+  }
+  setspeicher(data) {
+    data.forEach(element => {
+      this.speicher.push(element);
+    });
+  }
+
+  getspeicher() {
+    return this.speicher;
   }
  
 }
